879e31a46810acdbf3d933b97e9cedeb06961734
auzele <vauzele@gmail.com>
2025-05-03 21:38:03 +0300

    Add files via upload

---
+++ SMP_UNET/dataset.py
@@ -0,0 +1,25 @@
+import os
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+class BuildingDataset(Dataset):
+    def __init__(self, image_dir, mask_dir):
+        self.image_dir = image_dir
+        self.mask_dir = mask_dir
+        self.images = os.listdir(image_dir)
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.image_dir, self.images[idx])
+        mask_path = os.path.join(self.mask_dir, self.images[idx])
+        image = Image.open(img_path).convert("RGB")
+        mask = Image.open(mask_path).convert("L")
+
+        image = transforms.ToTensor()(image)
+        mask = transforms.ToTensor()(mask)
+        mask = (mask > 0).float()  # binārā maska
+
+        return image, mask
