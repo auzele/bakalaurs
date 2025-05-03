d6f6dfc82f6b9bd7e6bc39f44a9c79e73db8e89c
auzele <vauzele@gmail.com>
2025-05-03 22:02:19 +0300

    Update test.py

--- SMP_DEEPLABV3+/test.py
+++ SMP_DEEPLABV3+/test.py
@@ -1,22 +1,22 @@
+# Model architecture and training loop inspired by:
+# Iakubovskii, P. (2019). Segmentation Models Pytorch. GitHub repository.
+# https://github.com/qubvel/segmentation_models.pytorch
+
+
 import os
 import torch
 from torchvision import transforms
 from PIL import Image
 import segmentation_models_pytorch as smp
 
-# =================== PARAMETRI ===================
+#PARAMETERS
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-MODEL_PATH = "deeplabv3+_resnet50.pth"  # Saglabātā modeļa fails
-TEST_IMAGES_DIR = "/Users/viktorijaauzele/PycharmProjects/SMP_DEEPLABV3+/test/input"    # Mape ar test attēliem
-OUTPUT_DIR = "/Users/viktorijaauzele/PycharmProjects/SMP_DEEPLABV3+/test/result"  # Kur saglabāt maskas
+MODEL_PATH = "best_deeplabv3+_resnet50.pth"  
+TEST_IMAGES_DIR = ".../test/images"    
+OUTPUT_DIR = ".../test/result" 
 
-# =================== TRANSFORMĀCIJAS ===================
-transform = transforms.Compose([
-    transforms.Resize((256, 256)),
-    transforms.ToTensor(),
-])
 
-# =================== MODELA IELĀDE ===================
+#MODEL
 model = smp.DeepLabV3Plus(
     encoder_name="resnet50",
     encoder_weights=None,
@@ -28,7 +28,7 @@ model.load_state_dict(torch.load(MODEL_PATH, map_location=DEVICE))
 model = model.to(DEVICE)
 model.eval()
 
-# =================== TEST ATTĒLU PROGNOZĒŠANA ===================
+#TEST
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 test_images = os.listdir(TEST_IMAGES_DIR)
@@ -36,7 +36,7 @@ test_images = os.listdir(TEST_IMAGES_DIR)
 for img_name in test_images:
     img_path = os.path.join(TEST_IMAGES_DIR, img_name)
     image = Image.open(img_path).convert("RGB")
-    input_tensor = transform(image).unsqueeze(0).to(DEVICE)  # (1, 3, H, W)
+    input_tensor = transform(image).unsqueeze(0).to(DEVICE)  
 
     with torch.no_grad():
         output = model(input_tensor)
@@ -50,6 +50,6 @@ for img_name in test_images:
     save_path = os.path.join(OUTPUT_DIR, f"mask_{img_name}")
     pred_mask.save(save_path)
 
-    print(f"Saglabāts: {save_path}")
+    print(f"Saved: {save_path}")
 
-print("\nTests pabeigts!")
+print("\n Finished!")
